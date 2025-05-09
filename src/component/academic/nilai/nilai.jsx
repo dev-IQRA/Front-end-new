a627314689074ca63a60c1621624f7de375b87dd
Cokorda Gede Satria Widnyana Putra <coksatria2005@gmail.com>
2025-05-09 16:26:35 +0700

    Revisi pengerjaan nilai.jsx

--- src/component/academic/nilai/nilai.jsx
+++ src/component/academic/nilai/nilai.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./nilai.css";
-const Nilai = () => {
-  return (
-    <div className="nilai-container">
-      <h1>Nilai</h1>
-      <div className="nilai-content">
-        {/* Consider adding actual grade display table or cards here */}
-        <p>Grade information will be displayed here</p>
-        </div>
-    </div>
-  );
-};
-export default Nilai;
+import Sidebar from "../../sidebar/sidebar.jsx";
+
+const dataPengetahuan = [
+  { no: 1, kode: "MAT2023", mapel: "Matematika", semester: 4, huruf: "A", nilai: 92 }, 
+  { no: 2, kode: "BIO2023", mapel: "Biologi", semester: 4, huruf: "B", nilai: 85 },
+  { no: 3, kode: "FIS2023", mapel: "Fisika", semester: 4, huruf: "B", nilai: 87 },
+  { no: 4, kode: "KIM2023", mapel: "Kimia", semester: 4, huruf: "C", nilai: 67 },
+  { no: 5, kode: "ENG2023", mapel: "Bahasa Inggris", semester: 4, huruf: "A", nilai: 90 }
+];
+
+const dataKeterampilan = [ 
+  { no: 1, kode: "MAT2023", mapel: "Matematika", semester: 4, huruf: "A", nilai: 92},
+  { no: 2, kode: "BIO2023", mapel: "Biologi", semester: 4, huruf: "B", nilai: 85 },
+  { no: 3, kode: "FIS2023", mapel: "Fisika", semester: 4, huruf: "B", nilai: 87 },
+  { no: 4, kode: "KIM2023", mapel: "Kimia", semester: 4, huruf: "C", nilai: 67 },
+  { no: 5, kode: "ENG2023", mapel: "Bahasa Inggris", semester: 4, huruf: "A", nilai: 90 }
+]
