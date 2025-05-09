ced656afe0190392c84b94536bf03215c7739d3f
Cokorda Gede Satria Widnyana Putra <coksatria2005@gmail.com>
2025-05-09 09:32:17 +0700

    Update kehadiran.jsx

--- src/component/academic/kehadiran/kehadiran.jsx
+++ src/component/academic/kehadiran/kehadiran.jsx
@@ -1,2 +1,14 @@
+import React from "react";
+import "./kehadiran.css";
 
+const Kehadiran = () => {
+  return (
+    <div className="Kehadiran-container">
+      <div className="kehadiran-content">
+        {/* Add attendance tracking interface here */}
+        <p>Attendance information will be displayed here</p>
+      </div>
+    </div>
+  );
+};
 export default Kehadiran;
